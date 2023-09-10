7d8bee56c66eeee219a30c528f9f1f08b417af31
jeevan <jeevan@localhost.localdomain>
2023-09-10 07:22:13 -0400

    Checking Git hook

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -3,8 +3,8 @@
 # Copy the new jar to the build location
 cp -f java-app/target/*.jar jenkins/build/
 
-echo "**********************************************"
-echo "************ Building Docker Images **********"
-echo "**********************************************"
+echo "***********************************************"
+echo "************* Building Docker Images **********"
+echo "***********************************************"
 
 cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache

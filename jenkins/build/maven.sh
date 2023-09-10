99d3b48a587c1714ab39528b758ade8011fe6a01
jeevan <jeevan@localhost.localdomain>
2023-09-10 03:39:08 -0400

    Fixes workspace path for jobs execution

--- jenkins/build/maven.sh
+++ jenkins/build/maven.sh
@@ -4,4 +4,6 @@ echo "***************************************"
 echo "*********** Building Jar **************"
 echo "***************************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven "$@"
+WORKSPACE=/home/jeevan/jenkins_home/workspace/pipeline-docker-maven
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven "$@"
